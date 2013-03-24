76d379ebb8d5482ac03bebe1b520c119ba919f0b
joseEduardoGaytan <joseEduardoGaytan@gmail.com>
2013-03-23 18:52:05 -0600

    Agregado un nuevo branch para las pruebas, banch llamado pruebas, en el se anexa el código utilizado para hacer el fork o la bifurcación de los filtros

---
+++ src/PruebaFork/Plumber.java
@@ -0,0 +1,56 @@
+package PruebaFork;
+
+
+//import mx.uamcimat.a1.ejemplo.SourceFilter; // se importa la clase SourceFilter con el fin de no crearla de nueva cuenta
+
+/**
+ * Como se modifico la Superclase FilterFramework, es necesario utilizar de nueva cuenta el archivo
+ * SourceFilter
+ * @author Equipo Zac
+ *
+ */
+
+public class Plumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/****************************************************************************
+		* Aqui instanciamos los filtros
+		****************************************************************************/
+
+		SourceFilter filter1 = new SourceFilter();
+		FarenheitToCelsiusFilter tempFilter = new FarenheitToCelsiusFilter();
+		FeetToMetersFilter altFilter = new FeetToMetersFilter();
+		SinkFileFilter sinkFilter = new SinkFileFilter();
+		CopyOfSinkFileFilter sinkFilter2 = new CopyOfSinkFileFilter();
+
+		/****************************************************************************
+		* Aqui conectamos los filtros empezando con el filtro pozo (filter3) el cual
+		* conectamos al filter2 que es el filtro del medio. Posteriormente conectamos filter2
+		* al filtro fuente (filter1).
+		****************************************************************************/
+
+		sinkFilter2.Connect(tempFilter);
+		sinkFilter.Connect(altFilter); //conectar el puerto de entrada del Sink a la salida del Filtro de altitud
+		tempFilter.Connect(filter1); 	//Conectar el puerto de entrada del Filtro de Temp a la salida del Source
+		altFilter.Connect(filter1); //Conectar el puerto de entrada del Filtro de Altura con la salida del Filtro de Temperatura		
+		
+				
+		/****************************************************************************
+		* Aqui arrancamos los filtros. En realidad,... es  un poco aburrido.
+		****************************************************************************/
+
+		filter1.start();		
+		tempFilter.start();
+		altFilter.start();		
+		sinkFilter.start();
+		sinkFilter2.start();
+		
+
+	}
+
+}
